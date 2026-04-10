ca2ac6191a87a37a12df0e8f343cb47d59c05650
thankiprem73 <thanki.prem73@gmail.com>
2026-04-11 00:09:29 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Protoflexi</h1>
-
+<h1 className="text-red-500 text-4xl">
+  TEST UI
+</h1>
       <a href="/upload">Go to Upload</a>
       <br />
       <a href="/dashboard">Go to Dashboard</a>
