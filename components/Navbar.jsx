4ac02613f013ad27b34f4321e448b2f6dcb637bf
thankiprem73 <thanki.prem73@gmail.com>
2026-04-10 22:27:56 +1000

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,1 +1,10 @@
-
+// components/Navbar.jsx
+export default function Navbar() {
+  return (
+    <div style={{ padding: 20, background: "#eee" }}>
+      <a href="/">Home</a> |{" "}
+      <a href="/upload">Upload</a> |{" "}
+      <a href="/dashboard">Dashboard</a>
+    </div>
+  );
+}
