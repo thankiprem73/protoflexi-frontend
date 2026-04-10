d8df41a45aa36c875952a843e398a10bb650112c
thankiprem73 <thanki.prem73@gmail.com>
2026-04-11 00:13:46 +1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,24 @@
+import "./globals.css";
 import Navbar from "../components/Navbar";
 
 export const metadata = {
   title: "Protoflexi",
+  description: "PCB Prototyping Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
-        <Navbar />   {/* 👈 THIS is “including it” */}
-        {children}   {/* 👈 page content */}
+    <html lang="en">
+      <body className="bg-gray-50 text-gray-900">
+
+        {/* GLOBAL NAVBAR */}
+        <Navbar />
+
+        {/* PAGE CONTENT */}
+        <main className="min-h-screen">
+          {children}
+        </main>
+
       </body>
     </html>
   );
