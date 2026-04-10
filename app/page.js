bbe7feb82822ee99f4d325a4f24156621f8c1f65
thankiprem73 <thanki.prem73@gmail.com>
2026-04-10 22:26:11 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,28 +1,11 @@
-"use client";
-import { useState } from "react";
-import { API } from "@/lib/api";
-
-export default function Upload() {
-  const [file, setFile] = useState(null);
-  const [data, setData] = useState([]);
-
-  const uploadFile = async () => {
-    const formData = new FormData();
-    formData.append("file", file);
-
-    const res = await API.post("/upload", formData);
-    setData(res.data);
-  };
-
+export default function Home() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Upload BOM</h1>
+      <h1>Protoflexi</h1>
 
-      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-
-      <button onClick={uploadFile}>Upload</button>
-
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <a href="/upload">Go to Upload</a>
+      <br />
+      <a href="/dashboard">Go to Dashboard</a>
     </div>
   );
 }
