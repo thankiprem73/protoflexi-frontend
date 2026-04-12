0d7d9646a8eabde20e2bf5db99f232ccfd004773
thankiprem73 <thanki.prem73@gmail.com>
2026-04-12 15:03:27 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,24 +2,8 @@ import QuoteForm from "../components/QuoteForm";
 
 export default function Home() {
   return (
-    <div className="container">
-      {/* HERO */}
-      <div className="hero">
-        <div className="left">
-          <h1>Prototype. Test. Launch.</h1>
-          <p>PCB Prototyping & BOM Sourcing for Australia & New Zealand</p>
-
-          <div className="features">
-            <span>⚡ Fast turnaround</span>
-            <span>📦 Full BOM sourcing</span>
-            <span>🇦🇺 Local support</span>
-          </div>
-        </div>
-
-        <div className="right">
-          <QuoteForm />
-        </div>
-      </div>
-    </div>
+    <main className="p-10">
+      <QuoteForm />
+    </main>
   );
 }
